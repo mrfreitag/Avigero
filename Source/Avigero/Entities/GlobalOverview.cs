6fd21109cd43c9104a6bdb820909000341e4bda6
Richard Freitag <richard@mrfreitag.com>
2011-04-13 09:00:09 -0400

    Added class file GlobalOverview.cs which will contain information about all products, components, severities, etc.

---
+++ Source/Avigero/Entities/GlobalOverview.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Avigero.Entities
+{
+    enum Severities
+    {
+        enhancement = 0,
+        trivial,
+        minor,
+        normal,
+        major,
+        critical
+    }
+    /// <summary>
+    /// Global Overview contains, as the name implies, overview information about all products
+    /// and components of the project.
+    /// </summary>
+    class GlobalOverview
+    {
+        private static string m_Project;
+        private static List<string> m_Products;
+        private static List<string> m_Components;
+
+        /// <summary>
+        /// Count of bug severities for histogram
+        /// </summary>
+        private static int[] m_BugSeverities = new int[6];
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Initialize
+        {
+           
+        }
+    }
+}
